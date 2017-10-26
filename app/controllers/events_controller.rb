4e488c703d3fc7a6030d6778ace2bda908eec0c4
Clayton <cjsweeten101@gmail.com>
2017-10-25 23:33:46 -0600

    login filter

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+	before_action :logged_in, only: [:attend, :create]
 
 	def index
 		@events = Event.all
@@ -31,6 +32,13 @@ class EventsController < ApplicationController
 	end
 
 	private
+
+		def logged_in
+			unless logged_in?
+				flash[:error] = "Please login first"
+				redirect_to login_url
+			end
+		end
 
 		def event_params
 			params.require(:event).permit(:title, :date, :location, :description)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	root 'static_pages#home'
+	root 'events#index'
 
 	get    '/login',  to: 'sessions#new'
 	post   '/login',  to: 'sessions#create'
